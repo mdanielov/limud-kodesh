9bdbe5cfcf126846785275321494b3a6717a8192
samzadel <samzadel@hotmail.fr>
2019-07-18 22:06:50 +0300

    Correction of code which converts json tanach to xml

---
+++ Test-tanach/main.js
@@ -0,0 +1,96 @@
+var fs = require('fs')
+
+
+fs.readdir('./all_tanach_in_json', (err, dataTxt) => {
+    if (err) throw err;
+    for (var txt in dataTxt) {
+        var data = fs.readFileSync(`./all_tanach_in_json/${dataTxt[txt]}`)
+
+        var newdata = JSON.parse(data)
+
+        var file_no_extension = dataTxt[txt].replace(/\.[^\.]+$/, '')
+
+        var result = [];
+
+        for (var i in newdata) {
+            result.push([newdata[i]]);
+
+        }
+
+        var result2 = []
+
+        var stmt = '';
+
+        for (var i in result) {
+
+            for (var a in result[i]) {
+
+                for (var e in result[i][a]) {
+
+                    for (var z in result[i][a][e]) {
+
+                        var numberLineChapter= result[i][a][e].length -1
+                        result2.push(result[i][a][e][z].split(','))
+                        var sentence = result[i][a][e][z].split(' ');
+                        
+
+                        for (var w in sentence) {
+                            if (z == 0 && w == 0) {
+                                stmt += `<Word Book='${file_no_extension}' StartPerekBeforeWord='true' Chapter='${1 + parseInt(e)}' Verse='${parseInt(z) + 1}' WordSequence='${parseInt(w) + 1}'>${sentence[w]}</Word>${'\n\r'}`
+                                continue
+                            }
+                            if(z == numberLineChapter && w == sentence.length -1 && sentence[w] == '(פ)' ){
+                                continue
+                            }
+                            if(z == numberLineChapter && w == sentence.length -1 && sentence[w] == '(ס)' ){
+                                continue
+                            }
+                            if(z == numberLineChapter && w == sentence.indexOf('(פ)') - 1){
+                                stmt += `<Word Book='${file_no_extension}' EndPerekAfterWord='true' Chapter='${Number(e) + 1}' Verse='${Number(z) + 1}' WordSequence='${sentence.length-1}' IsParashaPtuchah='true' >${sentence[w]}</Word>${'\n\r'}`
+                                continue
+                            }
+                            if(z == numberLineChapter && w == sentence.indexOf('(ס)') - 1){
+                                stmt += `<Word Book='${file_no_extension}' EndPerekAfterWord='true' Chapter='${Number(e) + 1}' Verse='${Number(z) + 1}' WordSequence='${sentence.length-1}' IsParashaPtuchah='true' >${sentence[w]}</Word>${'\n\r'}`
+                                continue
+                            }
+                            if(z == numberLineChapter && w == sentence.length -1 && sentence[w] != '(פ)'   ){
+                                stmt += `<Word Book='${file_no_extension}' EndPerekAfterWord='true' Chapter='${Number(e) + 1}' Verse='${Number(z) + 1}' WordSequence='${sentence.length}' >${sentence[w]}</Word>${'\n\r'}`
+                                continue
+                            }
+                            if(z == numberLineChapter && w == sentence.length -1 && sentence[w] != '(ס)'   ){
+                                stmt += `<Word Book='${file_no_extension}' EndPerekAfterWord='true' Chapter='${Number(e) + 1}' Verse='${Number(z) + 1}' WordSequence='${sentence.length}' >${sentence[w]}</Word>${'\n\r'}`
+                                continue
+                            }
+
+                            else{
+                                if (sentence[w] == '(פ)' || sentence[w] == '(ס)') {
+                                    continue
+                                }
+                                if (w == sentence.indexOf('(פ)') - 1 && z != numberLineChapter) {
+                                    stmt += `<Word Book='${file_no_extension}' Chapter='${1 + parseInt(e)}' Verse='${parseInt(z) + 1}' WordSequence='${parseInt(w) + 1}' IsParashaPtuchah='true' >${sentence[w]}</Word>${'\n\r'}`
+                                    continue
+                                }
+                                stmt += `<Word Book='${file_no_extension}' Chapter='${1 + parseInt(e)}' Verse='${parseInt(z) + 1}' WordSequence='${parseInt(w) + 1}' >${sentence[w]}</Word>${'\n\r'}`
+
+                            }                            
+
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+
+        var FinalStatement = `<Tanakh>${'\n\r'}${stmt}</Tanakh>`
+        var file = dataTxt[txt].replace(/\.[^\.]+$/, '.xml');
+
+        fs.writeFile(`./Tanach-xml/${file}`, FinalStatement, (err) => {
+            if (err) throw err
+        })
+    }
+});
+
+
+
