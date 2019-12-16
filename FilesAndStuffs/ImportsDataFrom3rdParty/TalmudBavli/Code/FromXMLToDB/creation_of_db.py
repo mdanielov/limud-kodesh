bd297e26cf294f9e8d59402bd6643b9bad08e4c6
samzadel <samzadel@hotmail.fr>
2019-12-16 17:15:56 +0200

    Python code creation DB and Tables and insertion data into tables

--- FilesAndStuffs/ImportsDataFrom3rdParty/TalmudBavli/Code/FromXMLToDB/creation_of_db.py
+++ FilesAndStuffs/ImportsDataFrom3rdParty/TalmudBavli/Code/FromXMLToDB/creation_of_db.py
@@ -33,24 +33,36 @@ creation_table('C:\\Git\\limud-kodesh\\ki_metzion\\schema\\TABLES\\SEDER.sql')
 creation_table('C:\\Git\\limud-kodesh\\ki_metzion\\schema\\TABLES\\TALMUD_SEDER.sql')
 creation_table('C:\\Git\\limud-kodesh\\ki_metzion\\schema\\TABLES\\MASSECHET.sql')
 creation_table('C:\\Git\\limud-kodesh\\ki_metzion\\schema\\TABLES\\TALMUD_MASSECHET.sql')
+creation_table('C:\\Git\\limud-kodesh\\ki_metzion\\schema\\TABLES\\MASSECHET_PEREK.sql')
+
+def insert_data(filename):    
+    # Open and read the file as a single buffer
+    fd = open(filename, 'r', encoding="utf8")
+    sqlFile = fd.read()
+    fd.close()
+
+    # all SQL commands (split on ';')
+    sqlCommands = sqlFile.split(';')
+    # print(sqlCommands)
+
+    # Execute every command from the input file
+    for command in sqlCommands:
+        with sql_connect.conn:
+            cur2 = sql_connect.cursor.execute(command)
+            cur2.commit()
+
+insert_data('C:\\Git\\limud-kodesh\\ki_metzion\\data\\toshba.sql')
+insert_data('C:\\Git\\limud-kodesh\\ki_metzion\\data\\talmud_type.sql')
+insert_data('C:\\Git\\limud-kodesh\\ki_metzion\\data\\seder.sql')
+insert_data('C:\\Git\\limud-kodesh\\ki_metzion\\data\\talmud_seder.sql')
+insert_data('C:\\Git\\limud-kodesh\\ki_metzion\\data\\massechet.sql')
+insert_data('C:\\Git\\limud-kodesh\\ki_metzion\\data\\talmud_massechet.sql')
 
 
 
 
 
-    
 
-  
-
-
-
-# def create_query_string(sql_full_path):
-#     with open(sql_full_path, 'r') as f_in:
-#         lines = f_in.read()
-#         # remove any common leading whitespace from every line
-#         query_string = textwrap.dedent("""{}""".format(lines))
-#     print(query_string)
-#     return query_string
 
 
 
