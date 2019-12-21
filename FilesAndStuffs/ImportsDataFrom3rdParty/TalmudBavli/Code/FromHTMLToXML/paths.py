310af5f3b0952747ebf47a9e9052a15e2c5969a0
Mordechai Danielov <mdanielov@zevedtov.co.il>
2019-12-21 20:36:28 +0200

    add shas XML and fix paths

--- FilesAndStuffs/ImportsDataFrom3rdParty/TalmudBavli/Code/FromHTMLToXML/paths.py
+++ FilesAndStuffs/ImportsDataFrom3rdParty/TalmudBavli/Code/FromHTMLToXML/paths.py
@@ -1,8 +1,9 @@
 import platform
 
 if platform.platform().__contains__("Windows"):
-    output_base_input_dir = "C:\\Git\\kodesh_easy_learn\\reference\\shas_html\\"
-    output_base_output_dir = "C:\\Git\\kodesh_easy_learn\\reference\\parsed_shas\\"
+    output_base_input_dir = "C:\\Git\\limud-kodesh\\FilesAndStuffs\\ImportsDataFrom3rdParty\\TalmudBavli\\Code\\shas_html\\"
+    output_base_output_dir = "C:\\Git\\limud-kodesh\\FilesAndStuffs\\ImportsDataFrom3rdParty\\TalmudBavli\\Code\\shas_xml\\"
     output_dir_separator = "\\"
     download_base_dir = "E:\\kodesh\\hebrewbooks-shas\\"
+
 
