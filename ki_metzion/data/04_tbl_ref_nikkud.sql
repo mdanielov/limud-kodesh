22914dab9d8e62f21c5cf784945ab7938d35ed16
Michaelben92 <michaelbenchimol92@gmail.com>
2020-01-13 18:50:42 +0200

    Add new table creation script and data insert script for ref_letter and ref_nikkud

---
+++ ki_metzion/data/04_tbl_ref_nikkud.sql
@@ -0,0 +1,20 @@
+USE KiMeTzion
+GO
+
+INSERT INTO dbo.TBL_REF_NIKKUD 
+VALUES 
+('Kammats','קמץ','אָ'),
+('Patach','פתח','אַ'),
+('Segol','סגל','אֶ'),
+('Tsira','צירה','אֵ'),
+('Chirik','שיריק','אִ'),
+('Cholem Chaser','חולם חסר','אֹ'),
+('Cholem','חולם','אוֹ'),
+('Shuruk','שורוק','אוּ'),
+('Kubutz','קובוץ','אֻ'),
+('Sheva','שוה','אְ'),
+('Chataf Petach','חטף פתח','אֲ'),
+('Chataf Kamats','חטף','אֳ'),
+('Chataf Segol','חטף פתח','אֱ'),
+('Dagesh','דגש','בּ')
+GO
