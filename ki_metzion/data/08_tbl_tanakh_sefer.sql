0c1d0405385e11f950e45845cbfff68587bcfda6
Michaelben92 <michaelbenchimol92@gmail.com>
2020-01-16 15:25:44 +0200

    Add data script data insert for tanakh sefer, tanakh category, and add new table script

---
+++ ki_metzion/data/08_tbl_tanakh_sefer.sql
@@ -0,0 +1,47 @@
+USE KiMeTzion
+GO
+
+SET NOCOUNT ON;
+
+INSERT INTO dbo.TBL_TANAKH_SEFER
+VALUES
+('1','בראשית','Bereshit'),
+('1','שמות','Shemot'),
+('1','ויקרא','Vaykra'),
+('1','במדבר','Bamidbar'),
+('1','דברים','Devarim'),
+('2','יהושע','Yehosua'),
+('2','שופטים','Shoftim'),
+('2','שמואל א','Shmuel Aleph'),
+('2','שמואל ב','Shmuel Beth'),
+('2','מלכים א','Melachim Aleph'),
+('2','מלכים ב','Melachim Beth'),
+('3','ישעיהו','Yeshayahu'),
+('3','ירמיהו','Yirmiyahu'),
+('3','יחזקאל','Yehezkel '),
+('3','הושע','Hosea'),
+('3','יואל','Yoel'),
+('3','עמוס','Amos'),
+('3','עבדיה','Ovadia'),
+('3','יונה','Yona'),
+('3','מיכה','Mikha'),
+('3','נחום','Nakhum'),
+('3','חבקוק','Habakkuk'),
+('3','צפניה','Zephaniah'),
+('3','חגי','Hagay'),
+('3','זכריה','Zekharia'),
+('3','מלאכי','Melakhi'),
+('4','רות','Ruth'),
+('4','שיר השירים','Shir Hashirim'),
+('4','קהלת','Kohelet'),
+('4','איכה','Eikha'),
+('4','אסתר','Ester'),
+('5','תהלים','Tehelim'),
+('5','איוב','Yiov'),
+('5','משלי','Mishlei'),
+('6','דברי הימים א','Divrei Hyamim Aleph'),
+('6','דברי הימים ב','Divrei Hyamim Beth'),
+('6','דניאל','Daniel'),
+('6','עזרא','Ezra'),
+('6','נחמיה','Nekhemia')
+GO
