41d2cfeca4a279bc56de179e0f0021c8cd02ded3
Michaelben92 <michaelbenchimol92@gmail.com>
2020-01-19 18:36:54 +0200

    update script insert_data_to_tbl_perek_pasuk.py

---
+++ database/DB_Table_creation/insert_data_to_tbl_perek_pasuk.py
@@ -0,0 +1,13 @@
+import xml.etree.cElementTree as ET
+import os
+
+
+
+parent_dir = "C:\\Git\\limud-kodesh\\ImportsData\\Tanakh\\FromSefaria\\XMLFilesProcessed"
+
+
+xml_list = os.listdir(parent_dir)
+
+for xml in 
+
+print(xml_list)
