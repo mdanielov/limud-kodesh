910811bd71ccef313b993c82abc204f9bd3719ee
Michaelben92 <michaelbenchimol92@gmail.com>
2020-01-20 13:22:16 +0200

    Update books name for table tanakh_sefer, update tbl_complete_massechet_word, update insert data to perek_pasuk

--- database/DB_Table_creation/insert_data_to_tbl_perek_pasuk.py
+++ database/DB_Table_creation/insert_data_to_tbl_perek_pasuk.py
@@ -8,6 +8,24 @@ parent_dir = "C:\\Git\\limud-kodesh\\ImportsData\\Tanakh\\FromSefaria\\XMLFilesP
 
 xml_list = os.listdir(parent_dir)
 
-for xml in 
+chapter_num = []
+pasuk_num = []
 
-print(xml_list)
+for xml_file in xml_list:
+
+    book_name = None
+    del chapter_num[:]
+    del pasuk_num[:]
+
+    xml_path = parent_dir + "\\" + xml_file
+
+    tree = ET.ElementTree(file=xml_path)
+
+    for elem in tree.iter():
+        if elem.tag == 'Word' and book_name is None:
+            book_name = elem.attrib['Book']
+            # if elem.attrib == 'Book' and book_name is None:
+            #     print(elem.attrib['Book'])
+            #     book_name = elem.attrib['Book']
+
+    print(book_name)
