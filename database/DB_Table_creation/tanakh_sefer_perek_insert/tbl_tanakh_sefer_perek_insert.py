d58f3bfadc031b13b3ad3df4b25b8af6d4e5c81c
Michaelben92 <michaelbenchimol92@gmail.com>
2020-01-20 18:12:00 +0200

    update tanakh sefer perek insert script

--- database/DB_Table_creation/tanakh_sefer_perek_insert/tbl_tanakh_sefer_perek_insert.py
+++ database/DB_Table_creation/tanakh_sefer_perek_insert/tbl_tanakh_sefer_perek_insert.py
@@ -4,14 +4,12 @@ import re
 import pyodbc
 
 config = configparser.ConfigParser()
-config.read(os.path.join(os.path.abspath(
-    os.path.dirname(__file__)), 'settings.ini'))
+config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'settings.ini'))
 database_name = config.get("SQL", "database_name")
 server = config.get("SQL", "server")
-csv_path = "C:\\Work\\Git\\limud-kodesh\\database\\DB_Table_creation\\tanakh_sefer_perek_insert\\tanakh_perek_list.csv"
+csv_path = os.path.join(os.path.abspath(os.path.dirname(__file__))) + "\\" + 'tanakh_perek_list.csv'
 
 sefer_id_list = []
-
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=' + server + ';'
@@ -29,60 +27,64 @@ def execute_query(query):
     return cursor
 
 
-execute_query(f"USE KiMeTzion;")
-
-# Append all Sefer id's to sefer_id_list []
-
-sql_select_Query = "select * from TBL_TANAKH_SEFER"
-cursor.execute(sql_select_Query)
-records = cursor.fetchall()
-i=0
-for row in records:
-    sefer_id_list.append(row[0])
-    i += 1
-print("sefer_id_list: ", sefer_id_list)
-
 
 def get_csv_content(file_csv):
-
     file_csv = csv_path
     with open(file_csv, encoding='utf-8') as f:
+
         lineList = f.readlines()
-        i=0
-        k=0
+
+        k = 0
+
         for elem in lineList:
+
             element = elem.replace('\n', '')
+
             mylist = str(element).split("|")
-            print(mylist[1])
+
+            print("working on " + mylist[0])
 
             # Append all prakim in that sefer to a list
+
             sql_select_Query = f"select PEREK_ID from TBL_TANAKH_PEREK where PEREK_NUM BETWEEN 1 AND {mylist[1]}"
             cursor.execute(sql_select_Query)
             records = cursor.fetchall()
-            i=0
+
+            i = 0
+
             perek_id_list = []
             for row in records:
                 perek_id_list.append(row[0])
                 i += 1
             # print("perek_id_list: ", perek_id_list)
-            i=0
-            
+
+            i = 0
 
             # Loop through every perek in the sefer
             while i < int(mylist[1]):
-                # print("i: ", i, " k: ", k)
-                # print(perek_id_list[i], sefer_id_list[k])            
                 query_string = f"INSERT INTO TBL_TANAKH_SEFER_PEREK (PEREK_ID, SEFER_ID) VALUES ({perek_id_list[i]}, {sefer_id_list[k]})"
                 cursor.execute(query_string)
                 i += 1
-            perek_id_list = []
+
             k += 1
 
 
-
 def main():
+
+    execute_query(f"USE KiMeTzion;")
+
+    # Append all Sefer id's to sefer_id_list []
+
+    sql_select_Query = "select * from TBL_TANAKH_SEFER"
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+    i = 0
+
+    for row in records:
+        sefer_id_list.append(row[0])
+        i += 1
+
     get_csv_content(csv_path)
-    return "hello"
 
 
 if __name__ == '__main__':
