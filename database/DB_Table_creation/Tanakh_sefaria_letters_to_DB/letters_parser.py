219c2a7443abcda4908b70bec93dd2d274f39958
samzadel <samzadel@hotmail.fr>
2020-01-29 17:57:18 +0200

    New table 06_tbl_ref_taam_name and update python code of insert letters,taam and nikkud in db

--- database/DB_Table_creation/Tanakh_sefaria_letters_to_DB/letters_parser.py
+++ database/DB_Table_creation/Tanakh_sefaria_letters_to_DB/letters_parser.py
@@ -13,10 +13,20 @@ tanakh_dir_list = os.listdir(current_dir_path)
 server = config.get("SQL", "server")
 table_names = config.get("SQL", "table_names").split(',')
 database_name = config.get("SQL", "database_name")
-csv_file_name = os.path.join(os.path.abspath(os.path.dirname(__file__))) + "\\" + config.get('CSV','csv_file_name')
+csv_file_name_letter = os.path.join(os.path.abspath(os.path.dirname(__file__))) + "\\" + config.get('CSV','csv_file_name_letter')
+csv_file_name_nikkud = os.path.join(os.path.abspath(os.path.dirname(__file__))) + "\\" + config.get('CSV','csv_file_name_nikkud')
+csv_file_name_taam = os.path.join(os.path.abspath(os.path.dirname(__file__))) + "\\" + config.get('CSV','csv_file_name_taam')
 
-if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name)):
-    os.remove(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name))
+
+if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name_letter)):
+    os.remove(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name_letter))
+
+if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name_nikkud)):
+    os.remove(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name_nikkud))
+    
+if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name_letter)):
+    os.remove(os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_file_name_nikkud))
+    
     
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=' + server + ';'
@@ -33,44 +43,71 @@ def execute_query(query):
 
 execute_query(f"USE {database_name};")
 
+def write_csv_file(file_name, textline):
+    
+    """
+    This function will write a new line into csv file in order to Bulk insert into TBL_MASSECHET_WORD table.
+    :param file_name: the csv file name
+    :param textline: the string that will be appended to the csv file
+    """
+    with open(file_name, "a", encoding='utf-8') as f:
+        f.write(f"{textline}\r")
 
-def get_word_id(word,book,chapter,verse,wordSequence):
+
+def get_word_id(book,chapter,verse,wordSequence):
     
     query = f"""SELECT TW.WORD_ID FROM TBL_TANAKH_WORD TW
-            JOIN TBL_TANAKH_PEREK_PASUK PP ON PP.PEREK_PASUK_ID = TW.PEREK_PASUK_ID
-            JOIN TBL_TANAKH_PASUK TP ON TP.PASUK_ID = PP.PASUK_ID
-            JOIN TBL_TANAKH_PEREK TPE ON TPE.PEREK_ID = PP.SEFER_PEREK_ID
-            JOIN TBL_TANAKH_SEFER_PEREK TSP ON TSP.SEFER_PEREK_ID = PP.SEFER_PEREK_ID
-            JOIN TBL_TANAKH_SEFER TS ON TS.SEFER_ID = TSP.SEFER_ID
-            WHERE TW.WORD = '{word}' AND TW.WORD_POSITION = '{wordSequence}' AND TP.PASUK_NUM ='{verse}' AND TPE.PEREK_NUM ='{chapter}' AND TS.SEFER_HEBREW_NAME = '{book}'"""        
-
+                JOIN TBL_TANAKH_PEREK_PASUK PP ON PP.PEREK_PASUK_ID = TW.PEREK_PASUK_ID
+                JOIN TBL_TANAKH_PASUK PA ON PA.PASUK_ID = PP.PASUK_ID
+                JOIN TBL_TANAKH_SEFER_PEREK SP ON SP.SEFER_PEREK_ID = PP.SEFER_PEREK_ID
+                JOIN TBL_TANAKH_PEREK PE ON PE.PEREK_ID = SP.PEREK_ID
+                JOIN TBL_TANAKH_SEFER S ON S.SEFER_ID = SP.SEFER_ID
+                WHERE PA.PASUK_NUM = '{verse}' AND PE.PEREK_NUM = '{chapter}' AND  TW.WORD_POSITION = '{wordSequence}' AND S.SEFER_ENGLISH_NAME = '{book}'"""        
+    
+    result_query = execute_query(query)
+    
+    for row in result_query:
+           get_word_id = row[0]
+    
+    return get_word_id
 
 def get_ref_letter_id(letter):
     
     query = f"SELECT LETTER_ID FROM TBL_REF_LETTER WHERE LETTER = '{letter}'"
 
-    #result_query = execute_query(query)
-    #
-    #for row in result_query:
-    #    get_ref_letter_id = row[0]
-    #
-    #return get_ref_letter_id
+    result_query = execute_query(query)
+    
+    get_ref_letter_id = ''
+    
+    for row in result_query:
+       get_ref_letter_id = row[0]
+    
+    return get_ref_letter_id
 
 def get_ref_nikkud_id(nikkud):
     
-    query = f"SELECT NIKKUD_ID FROM TBL_REF_NIKKUD WHERE NIKKUD = '{nikkud}'"
+    query = f"SELECT NIKKUD_ID FROM TBL_REF_NIKKUD WHERE UNICODE_VALUE = '{nikkud}'"
+    print(query)
+    
+    result_query = execute_query(query)
+    
+    get_ref_nikkud_id = []
     
     for row in result_query:
-        get_ref_nikkud_id = row[0]
-        
+        get_ref_nikkud_id.append(row[0]) 
+    
     return get_ref_nikkud_id
 
 def get_ref_taam_id(taam):
     
-    query = f"SELECT TAAM_ID FROM TBL_REF_TAAM WHERE TAAM= '{taam}'"
+    query = f"SELECT TAAM_ID FROM TBL_REF_TAAM WHERE UNICODE_VALUE= '{taam}'"
+    
+    result_query = execute_query(query)
+    
+    get_ref_taam_id = []
     
     for row in result_query:
-        get_ref_taam_id = row[0]
+        get_ref_taam_id.append(row[0])
         
     return get_ref_taam_id  
 
@@ -83,16 +120,21 @@ def get_letter_nikkud_taam(xml):
     
     for elem in tree.findall('Word'):
         
-        word_id = get_word_id(elem.text,elem.attrib.get('Book'),elem.attrib.get('Chapter'),elem.attrib.get('Verse'),elem.attrib.get('WordSequence'))
+        word_id = get_word_id(elem.attrib.get('Book'),elem.attrib.get('Chapter'),elem.attrib.get('Verse'),elem.attrib.get('WordSequence'))
 
         count = 1
         
-        for index,item in enumerate(elem.text):
-            
+        text = list(elem.text)
+        
+        for index,item in enumerate(text):
+           
+            print(text[index])
+           
             if re.match('[א-ת]',item):
                 ref_letter_id = get_ref_letter_id(item)
                 letter_position_in_word = count 
-                textline1 = f"|{word_id}|{ref_letter_id}|{letter_position_in_word}"    
+                textline1 = f"|{word_id}|{ref_letter_id}|{letter_position_in_word}" 
+                write_csv_file(csv_file_name_letter,textline1)
                 count +=1       
                 continue
                 
@@ -100,18 +142,23 @@ def get_letter_nikkud_taam(xml):
             items2 = re.sub("(B'\\\\)|('$)",'',items)
             items3 = items2.replace('\\','').replace('U','U+')
             
-            if get_ref_nikkud_id(items3) != '':
-                
+            if get_ref_nikkud_id(items3) != []:
                 nikkud_id = get_ref_nikkud_id(items3)
-                
+                print(nikkud_id)
+                if index > 0 and nikkud_id == [8,14]:
+                    if text[index-1] == 'ו':
+                        nikkud_id = 8
+                    else:
+                        nikkud_id = 14
                 textline2 = f"|{letter_id}|{nikkud_id}" 
-            
+                write_csv_file(csv_file_name_nikkud,textline2)
+
             else:
-                
-                taam_id = get_letter_nikkud_taam(items3)
-                
-                textline3 = f"|{letter_id}|{taam_id}"   
-            
+                taam_id = get_ref_taam_id(items3)
+                textline3 = f"|{letter_id}|{taam_id}"
+                write_csv_file(csv_file_name_taam,textline3)
+                 
+                 
         letter_id += 1
             
            
