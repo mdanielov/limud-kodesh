a6c9a3fd8dc647470a59f9a48f58ca98193db1e8
Michaelben92 <michaelbenchimol92@gmail.com>
2020-02-09 14:55:28 +0200

    update navbar and put into module.

--- UI/UI_MAIN_PROJECT/resources/views/Initial_list.blade.php
+++ UI/UI_MAIN_PROJECT/resources/views/Initial_list.blade.php
@@ -1,41 +1,20 @@
 <!DOCTPE html>
   <html>
   @extends('layout\head')
-
+  @include('layout\navbar')
   <head>
     <title>View Abbreviations</title>
     <link href="{{ URL::asset('css/Initial_list.css') }}" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   </head>
 
   <body>
 
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 5%;">
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <a href="#"><img src="{{ URL::asset("img/final_logo.jpg") }}"" width=" 150px" style="border-radius: 50px;" alt="bone_yerushalayim_logo"></a>
-        <ul class="navbar-nav" style="margin : 0 auto;">
-          <li class="nav-item active">
-            <a class="nav-link" href="#" style="margin: 0 40px 0 -100px">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" style="margin: 0 40px 0 40px">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" style="margin: 0 40px 0 40px">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" style="margin: 0 40px 0 40px">Disabled</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-
-
     <h1 class="text-center">ראשי תיבות <br><small class="text-muted">(Abbreviations)</small></h1>
 
 
-    <p class="lead text-center" style="margin-bottom: 5%;">Here are all the abbreviations contained in the talmud Bavli ! More than 1300 ! <br> click on the one of your choice to propose a definition</p>
+    <p class="lead text-center" style="margin-bottom: 5%;">Here are all the abbreviations contained in the talmud Bavli ! More than 1300 ! <br> <strong> Be a part of the project ! </strong> <br> We need your help to assign a definition to each one of the abbreviations contained in the talmud, according to their context in the gemara.
+      <br> Click on the one of your choice to propose a definition</p>
 
     <div style="width: 30%; margin : 0 auto;">
       <table class="table table-striped table-bordered table-sm data-table" style="text-align: center;" id="initial_table">
@@ -56,5 +35,78 @@
     </div>
 
   </body>
+
+  <script>
+    $(document).ready(function() {
+
+      $('#abbreviation').addClass('active');
+
+      // Setup - add a text input to each footer cell
+      $('#initial_table thead tr').clone(true).appendTo('#initial_table thead');
+      $('#initial_table thead tr:eq(1) th').each(function(i) {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" id="filter_input" />');
+
+        $('#filter_input', this).on('keyup change', function() {
+          
+          console.log(this.value);
+          console.log(table.column(i).search(this.value).draw())
+          if (table.column(i).search(this.value) == true) {
+            table
+              .column(i)
+              .search(this.value)
+              .draw();
+          }
+        });
+      });
+
+      var table = $('#initial_table').DataTable({
+        orderCellsTop: true,
+        fixedHeader: true,
+        bStateSave: false,
+        //"bPaginate": true,
+        //"bLengthChange": false,
+        //"bFilter": false,
+        //"bInfo": false,
+      });
+
+      (function() {
+
+        function removeAccents(data) {
+          if (data.normalize) {
+            // Use I18n API if avaiable to split characters and accents, then remove
+            // the accents wholesale. Note that we use the original data as well as
+            // the new to allow for searching of either form.
+            return data + ' ' + data
+              .normalize('NFD')
+              .replace(/[\u0300-\u036f]/g, '');
+          }
+
+          return data;
+        }
+
+        var searchType = jQuery.fn.DataTable.ext.type.search;
+
+        searchType.string = function(data) {
+          return !data ?
+            '' :
+            typeof data === 'string' ?
+            removeAccents(data) :
+            data;
+        };
+
+        searchType.html = function(data) {
+          return !data ?
+            '' :
+            typeof data === 'string' ?
+            removeAccents(data.replace(/<.*?>/g, '')) :
+            data;
+        };
+
+      }());
+
+
+    });
+  </script>
 
   </html>
