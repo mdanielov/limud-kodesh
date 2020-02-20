aa487cbcae24aaa745c4d6e1a9d6cede7c30351e
samzadel <samzadel@hotmail.fr>
2020-02-20 17:29:32 +0200

    Bat file

--- database/DB_Table_creation/Execute_all_script.bat
+++ database/DB_Table_creation/Execute_all_script.bat
@@ -9,5 +9,5 @@ python insert_data_to_tbl_perek_pasuk.py
 cd "C:\Git\limud-kodesh\database\DB_Table_creation\Tanakh_sefaria_word_to_DB\"
 python xml_tanakh_parser.py
 cd "C:\Git\limud-kodesh\database\DB_Table_creation\Tanakh_sefaria_letters_to_DB\"
-python letters.parser.py
+python letters_parser.py
 pause
