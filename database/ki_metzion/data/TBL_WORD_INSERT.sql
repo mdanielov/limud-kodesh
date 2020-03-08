30442f4ccf80d7c3b13fc2167603ac5405bbae0c
Michaelben92 <michaelbenchimol92@gmail.com>
2020-03-08 21:20:27 +0200

    add insert statement for TBL_WORD

---
+++ database/ki_metzion/data/TBL_WORD_INSERT.sql
@@ -0,0 +1,18 @@
+IF OBJECT_ID('tempdb..#TMP_WORD') IS NOT NULL DROP TABLE #TMP_WORD
+GO
+
+CREATE TABLE #TMP_WORD (WORD NVARCHAR(100))
+
+INSERT INTO #TMP_WORD (WORD)
+select DISTINCT WORD FROM TBL_TANAKH_WORD
+
+INSERT INTO #TMP_WORD (WORD)
+select distinct word from tbl_bavli_word
+
+INSERT INTO #TMP_WORD (WORD)
+select distinct word from TBL_YERUSHALMI_MASSECHET_WORD 
+
+INSERT INTO TBL_WORD
+select distinct word from #TMP_WORD where word not like '' order by 1
+
+select * from TBL_WORD
