5ddf27bb7e8e3acc851b15a1b9e4a2091f99c45d
Mordechai Danielov <mdanielov@zevedtov.co.il>
2020-10-06 17:57:14 +0300

    kimetzion as a database solution

---
+++ database/db_solution/kimetzion/dbo/Functions/bavliMatch.sql
@@ -0,0 +1,30 @@
+﻿
+CREATE FUNCTION [dbo].[bavliMatch] (
+    @sterm nvarchar(52)
+)
+RETURNS TABLE
+AS
+RETURN
+select top 100 percent
+MW.bavli_word_id
+,M.MASSECHET_NAME
+,MD.MASSECHET_ID
+,MP.PEREK_NAME
+,D.DAF_NAME
+--,D.DAF_NUM
+,D.AMUD_NAME
+--,D.AMUD_NUM
+,MW.row_id
+,MW.WORD_POSITION
+,(select word from TBL_BAVLI_WORD where bavli_word_id = MW.bavli_word_id + 1) [next_word]
+,MW.WORD
+,(select word from TBL_BAVLI_WORD where bavli_word_id = MW.bavli_word_id - 1) [previous_word]
+from TBL_BAVLI_WORD MW 
+JOIN TBL_MASSECHET_DAF MD ON MW.MASSECHET_DAF_ID = MD.MASSECHET_DAF_ID
+JOIN TBL_DAF D ON MD.DAF_AMUD_ID = D.DAF_AMUD_ID
+JOIN TBL_MASSECHET M ON M.MASSECHET_ID = MD.MASSECHET_ID
+JOIN TBL_MASSECHET_PEREK MP ON MW.PEREK_ID = MP.PEREK_ID AND MD.MASSECHET_ID = MP.MASSECHET_ID
+where
+1=1
+AND word = @sterm
+order by DAF_NUM,AMUD_NUM,row_id,WORD_POSITION
---
+++ database/db_solution/kimetzion/dbo/Tables/TBL_USER_BAVLI_WORD_DEF.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TBL_USER_BAVLI_WORD_DEF] (
+    [USER_BAVLI_WORD_ID] INT             IDENTITY (1, 1) NOT NULL,
+    [MASSECHET_NAME]     NVARCHAR (50)   NULL,
+    [DAF_NAME]           NVARCHAR (3)    NULL,
+    [AMUD_NAME]          NVARCHAR (1)    NULL,
+    [ROW_ID]             INT             NULL,
+    [WORD_POSITION]      INT             NULL,
+    [WORD]               NVARCHAR (50)   NULL,
+    [WORD_DEF]           NVARCHAR (3000) NULL,
+    CONSTRAINT [PK_USER_BAVLI_WORD_DEF_ID] PRIMARY KEY CLUSTERED ([USER_BAVLI_WORD_ID] ASC)
+);
+
